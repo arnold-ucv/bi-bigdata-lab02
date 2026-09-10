93ad044d51d553f71897229957bd7b65fe7bcf69
ARNOLD VILLANUEVA <avillanuevahu@ucvvirtual.edu.pe>
2026-09-10 08:32:20 -0500

    feat: add Unity Catalog schema changeset

---
+++ changelog/db.changelog-master.sql
@@ -0,0 +1,6 @@
+--liquibase formatted sql
+
+--changeset estudiante:001
+CREATE SCHEMA IF NOT EXISTS workspace.bi_lab_avillanuevahu
+COMMENT 'Laboratorio 02 - BI y Big Data - UCV';
+--rollback DROP SCHEMA IF EXISTS workspace.bi_lab_avillanuevahu;
