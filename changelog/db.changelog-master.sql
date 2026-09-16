35aca51f74bb9beec59358fc33b95983b91a486e
ARNOLD VILLANUEVA <avillanuevahu@ucvvirtual.edu.pe>
2026-09-15 19:07:08 -0500

    feat: add staging schema migration

--- changelog/db.changelog-master.sql
+++ changelog/db.changelog-master.sql
@@ -4,3 +4,7 @@
 CREATE SCHEMA IF NOT EXISTS workspace.bi_lab_avillanuevahu
 COMMENT 'Laboratorio 02 - BI y Big Data - UCV';
 --rollback DROP SCHEMA IF EXISTS workspace.bi_lab_avillanuevahu;
+--changeset estudiante:002
+CREATE SCHEMA IF NOT EXISTS workspace.bi_staging_ucv
+COMMENT 'Staging schema - BI and Big Data - Lab 03';
+--rollback DROP SCHEMA IF EXISTS workspace.bi_staging_ucv;
