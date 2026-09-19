2a1fed1943bfa631f6fda083ab41477d5f808f9f
ARNOLD VILLANUEVA <avillanuevahu@ucvvirtual.edu.pe>
2026-09-18 23:43:18 -0500

    test: induce checksum failure on changeset 002

--- changelog/db.changelog-master.sql
+++ changelog/db.changelog-master.sql
@@ -4,7 +4,8 @@
 CREATE SCHEMA IF NOT EXISTS workspace.bi_lab_avillanuevahu
 COMMENT 'Laboratorio 02 - BI y Big Data - UCV';
 --rollback DROP SCHEMA IF EXISTS workspace.bi_lab_avillanuevahu;
+
 --changeset estudiante:002
 CREATE SCHEMA IF NOT EXISTS workspace.bi_staging_bilab_avillanuevahu_s3
-COMMENT 'Staging schema - BI and Big Data - Lab 03';
+COMMENT 'Modificacion temporal para prueba de checksum - Lab 03';
 --rollback DROP SCHEMA IF EXISTS workspace.bi_staging_bilab_avillanuevahu_s3;
